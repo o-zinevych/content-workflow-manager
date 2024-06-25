a5c54a33dbd6388ab5df1d5b8343f7d2431669cb
Olha <olgazinevych@gmail.com>
2024-06-25 17:57:19 +0300

    add content urls file with home page link and index view

--- content/views.py
+++ content/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from content.models import ContentType, Staff, Task
+
+
+def index(request):
+    """View function for the home page."""
+    content_types_num = ContentType.objects.count()
+    staff_num = Staff.objects.count()
+    tasks_num = Task.objects.count()
+    context = {
+        "content_types_num": content_types_num,
+        "staff_num": staff_num,
+        "tasks_num": tasks_num,
+    }
+    return render(request, "content/index.html", context=context)
