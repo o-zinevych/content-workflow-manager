4b72c65ca91ab2dc5f4556e889cb5b30c08a04f3
Olha <olgazinevych@gmail.com>
2024-07-09 10:08:58 +0300

    test: add content type tests for required login and properly retrieved data

--- content/tests/test_views.py
+++ content/tests/test_views.py
@@ -7,6 +7,7 @@ from django.urls import reverse
 from content.models import ContentType, Task
 
 INDEX_URL = reverse("content:index")
+CONTENT_TYPE_URL = reverse("content:content-type-list")
 
 
 class PublicIndexViewTest(TestCase):
@@ -47,3 +48,30 @@ class PrivateIndexViewTest(TestCase):
         self.assertContains(response, ContentType.objects.count())
         self.assertContains(response, get_user_model().objects.count())
         self.assertContains(response, Task.objects.count())
+
+
+class PublicContentTypeViewTest(TestCase):
+    def test_login_required(self):
+        response = self.client.get(CONTENT_TYPE_URL)
+        self.assertNotEqual(response.status_code, 200)
+
+
+class PrivateContentTypeViewTest(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="test_user",
+            password="test1234"
+        )
+        self.client.force_login(self.user)
+
+    def test_retrieve_content_types(self):
+        ContentType.objects.create(name="article")
+        ContentType.objects.create(name="long-form video")
+        content_types = ContentType.objects.all()
+
+        response = self.client.get(CONTENT_TYPE_URL)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            list(response.context["content_type_list"]),
+            list(content_types)
+        )
