ff1d54ece34f8c26f87d2094317d3699432ba84a
Olha <olgazinevych@gmail.com>
2024-07-09 11:06:42 +0300

    test: add staff tests to check search form and querysets for different staff searches

--- content/tests/test_views.py
+++ content/tests/test_views.py
@@ -4,9 +4,9 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase, RequestFactory
 from django.urls import reverse
 
-from content.forms import PositionSearchForm
+from content.forms import PositionSearchForm, StaffSearchForm
 from content.models import ContentType, Task, Position
-from content.views import PositionListView
+from content.views import PositionListView, StaffListView
 
 INDEX_URL = reverse("content:index")
 CONTENT_TYPE_URL = reverse("content:content-type-list")
@@ -156,4 +156,38 @@ class PrivateStaffViewTest(TestCase):
         self.assertEqual(
             list(response.context["staff_list"]),
             list(staff)
+        )
+
+    def test_staff_search_form_is_present(self):
+        search_form = StaffSearchForm()
+        response = self.client.get(STAFF_URL)
+        self.assertIsInstance(
+            response.context["search_form"],
+            type(search_form)
+        )
+
+    def test_staff_get_queryset(self):
+        view = StaffListView()
+        request = RequestFactory().get(f"staff/?staff={self.test_username}")
+        view.request = request
+        queryset = view.get_queryset()
+        self.assertQuerysetEqual(
+            queryset,
+            get_user_model().objects.filter(username=self.test_username)
+        )
+
+        request = RequestFactory().get(f"staff/?staff={self.test_first_name}")
+        view.request = request
+        queryset = view.get_queryset()
+        self.assertQuerysetEqual(
+            queryset,
+            get_user_model().objects.filter(first_name=self.test_first_name)
+        )
+
+        request = RequestFactory().get(f"staff/?staff={self.test_last_name}")
+        view.request = request
+        queryset = view.get_queryset()
+        self.assertQuerysetEqual(
+            queryset,
+            get_user_model().objects.filter(last_name=self.test_last_name)
         )
