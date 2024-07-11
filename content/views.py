87dc73ca6210c4c9be2162c39dd19ef3e5d0d65d
Olha <olgazinevych@gmail.com>
2024-07-11 19:13:11 +0300

    refactor: move staff and position views to staff_manager/views.py

--- content/views.py
+++ content/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.db.models import Prefetch, Q
+from django.db.models import Q
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy, reverse
@@ -9,22 +9,17 @@ from django.views import generic
 
 from content.forms import (
     ContentTypeForm,
-    PositionForm,
-    PositionSearchForm,
-    StaffChangeForm,
-    StaffCreationForm,
-    StaffSearchForm,
     TaskForm,
     TaskSearchForm,
 )
-from content.models import ContentType, Staff, Task, Position
+from content.models import ContentType, Task
 
 
 @login_required
 def index(request: HttpRequest) -> HttpResponse:
     """View function for the home page."""
     content_types_num = ContentType.objects.count()
-    staff_num = Staff.objects.count()
+    staff_num = get_user_model().objects.count()
     tasks_num = Task.objects.count()
     context = {
         "content_types_num": content_types_num,
@@ -69,102 +64,6 @@ class ContentTypeDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = ContentType
     template_name = "content/content_type_confirm_delete.html"
     success_url = reverse_lazy("content:content-type-list")
-
-
-class PositionListView(LoginRequiredMixin, generic.ListView):
-    model = Position
-    paginate_by = 5
-
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super(PositionListView, self).get_context_data(**kwargs)
-        name = self.request.GET.get("name", "")
-        context["search_form"] = PositionSearchForm(initial={"name": name})
-        return context
-
-    def get_queryset(self):
-        queryset = Position.objects.all()
-        name = self.request.GET.get("name")
-        if name:
-            return queryset.filter(name__icontains=name)
-        return queryset
-
-
-class PositionDetailView(LoginRequiredMixin, generic.DetailView):
-    model = Position
-    queryset = Position.objects.prefetch_related("staff")
-
-
-class PositionCreateView(LoginRequiredMixin, generic.CreateView):
-    form_class = PositionForm
-    template_name = "content/position_form.html"
-    success_url = reverse_lazy("content:position-list")
-
-
-class PositionUpdateView(LoginRequiredMixin, generic.UpdateView):
-    model = Position
-    form_class = PositionForm
-    template_name = "content/position_form.html"
-    success_url = reverse_lazy("content:position-list")
-
-
-class PositionDeleteView(LoginRequiredMixin, generic.DeleteView):
-    model = Position
-    success_url = reverse_lazy("content:position-list")
-
-
-class StaffListView(LoginRequiredMixin, generic.ListView):
-    model = Staff
-    paginate_by = 7
-
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super(StaffListView, self).get_context_data(**kwargs)
-        staff = self.request.GET.get("staff", "")
-        context["search_form"] = StaffSearchForm(initial={"staff": staff})
-        return context
-
-    def get_queryset(self):
-        queryset = get_user_model().objects.prefetch_related("position")
-        staff = self.request.GET.get("staff")
-        if staff:
-            return queryset.filter(
-                Q(username__icontains=staff)
-                | Q(first_name__icontains=staff)
-                | Q(last_name__icontains=staff)
-            )
-        return queryset
-
-
-class StaffDetailView(LoginRequiredMixin, generic.DetailView):
-    model = Staff
-    queryset = get_user_model().objects.prefetch_related(
-        "position",
-        Prefetch("tasks", queryset=Task.objects.filter(is_finished=False))
-    )
-
-    def get_context_data(self, **kwargs):
-        context = super(StaffDetailView, self).get_context_data(**kwargs)
-        staff = self.get_object()
-        unfinished_tasks = staff.tasks.filter(is_finished=False)
-        context["unfinished_tasks"] = unfinished_tasks
-        return context
-
-
-class StaffCreateView(LoginRequiredMixin, generic.CreateView):
-    form_class = StaffCreationForm
-    template_name = "content/staff_form.html"
-    success_url = reverse_lazy("content:staff-list")
-
-
-class StaffUpdateView(LoginRequiredMixin, generic.UpdateView):
-    model = Staff
-    form_class = StaffChangeForm
-    template_name = "content/staff_form.html"
-    success_url = reverse_lazy("content:staff-list")
-
-
-class StaffDeleteView(LoginRequiredMixin, generic.DeleteView):
-    model = Staff
-    success_url = reverse_lazy("content:staff-list")
 
 
 class TaskListView(LoginRequiredMixin, generic.ListView):
--- staff_manager/views.py
+++ staff_manager/views.py
@@ -1,3 +1,111 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q, Prefetch
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from content.models import Task
+from staff_manager.forms import (
+    PositionForm,
+    PositionSearchForm,
+    StaffChangeForm,
+    StaffCreationForm,
+    StaffSearchForm,
+)
+from staff_manager.models import Position
+
+
+class PositionListView(LoginRequiredMixin, generic.ListView):
+    model = Position
+    paginate_by = 5
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(PositionListView, self).get_context_data(**kwargs)
+        name = self.request.GET.get("name", "")
+        context["search_form"] = PositionSearchForm(initial={"name": name})
+        return context
+
+    def get_queryset(self):
+        queryset = Position.objects.all()
+        name = self.request.GET.get("name")
+        if name:
+            return queryset.filter(name__icontains=name)
+        return queryset
+
+
+class PositionDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Position
+    queryset = Position.objects.prefetch_related("staff")
+
+
+class PositionCreateView(LoginRequiredMixin, generic.CreateView):
+    form_class = PositionForm
+    template_name = "content/position_form.html"
+    success_url = reverse_lazy("content:position-list")
+
+
+class PositionUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Position
+    form_class = PositionForm
+    template_name = "content/position_form.html"
+    success_url = reverse_lazy("content:position-list")
+
+
+class PositionDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Position
+    success_url = reverse_lazy("content:position-list")
+
+
+class StaffListView(LoginRequiredMixin, generic.ListView):
+    model = get_user_model()
+    paginate_by = 7
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(StaffListView, self).get_context_data(**kwargs)
+        staff = self.request.GET.get("staff", "")
+        context["search_form"] = StaffSearchForm(initial={"staff": staff})
+        return context
+
+    def get_queryset(self):
+        queryset = get_user_model().objects.prefetch_related("position")
+        staff = self.request.GET.get("staff")
+        if staff:
+            return queryset.filter(
+                Q(username__icontains=staff)
+                | Q(first_name__icontains=staff)
+                | Q(last_name__icontains=staff)
+            )
+        return queryset
+
+
+class StaffDetailView(LoginRequiredMixin, generic.DetailView):
+    model = get_user_model()
+    queryset = get_user_model().objects.prefetch_related(
+        "position",
+        Prefetch("tasks", queryset=Task.objects.filter(is_finished=False))
+    )
+
+    def get_context_data(self, **kwargs):
+        context = super(StaffDetailView, self).get_context_data(**kwargs)
+        staff = self.get_object()
+        unfinished_tasks = staff.tasks.filter(is_finished=False)
+        context["unfinished_tasks"] = unfinished_tasks
+        return context
+
+
+class StaffCreateView(LoginRequiredMixin, generic.CreateView):
+    form_class = StaffCreationForm
+    template_name = "content/staff_form.html"
+    success_url = reverse_lazy("content:staff-list")
+
+
+class StaffUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = get_user_model()
+    form_class = StaffChangeForm
+    template_name = "content/staff_form.html"
+    success_url = reverse_lazy("content:staff-list")
+
+
+class StaffDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = get_user_model()
+    success_url = reverse_lazy("content:staff-list")
