2aa1fb2e2a6cbd00c9c6a5f2101423cfe3e41bcd
Olha <olgazinevych@gmail.com>
2024-07-11 17:34:58 +0300

    refactor: move staff and position models to staff_manager app

--- content/models.py
+++ content/models.py
@@ -14,30 +14,6 @@ class ContentType(models.Model):
         return self.name
 
 
-class Position(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-
-    class Meta:
-        ordering = ["name"]
-
-    def __str__(self):
-        return self.name
-
-
-class Staff(AbstractUser):
-    position = models.ManyToManyField(Position, related_name="staff")
-
-    class Meta:
-        verbose_name_plural = "staff"
-
-    def __str__(self):
-        positions = ", ".join([pos.name for pos in self.position.all()])
-        if self.first_name and self.last_name:
-            return (f"{self.first_name} {self.last_name} "
-                    f"({positions}, @{self.username})")
-        return f"{self.username} ({positions})"
-
-
 class Task(models.Model):
     PRIORITIES = [
         ("0", "low"),
--- staff_manager/models.py
+++ staff_manager/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Staff(AbstractUser):
+    position = models.ManyToManyField(Position, related_name="staff")
+
+    class Meta:
+        verbose_name_plural = "staff"
+
+    def __str__(self):
+        positions = ", ".join([pos.name for pos in self.position.all()])
+        if self.first_name and self.last_name:
+            return (f"{self.first_name} {self.last_name} "
+                    f"({positions}, @{self.username})")
+        return f"{self.username} ({positions})"
