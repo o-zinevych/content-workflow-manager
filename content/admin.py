57f3dd6701fa1f287e7ad499e6ce8cdf6f4fc84c
Olha <olgazinevych@gmail.com>
2024-07-11 17:40:45 +0300

    refactor: move staffadmin to staff_manager/admin.py

--- content/admin.py
+++ content/admin.py
@@ -1,33 +1,8 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 
-from content.models import ContentType, Position, Staff, Task
+from content.models import ContentType, Task
 
 admin.site.register(ContentType)
-admin.site.register(Position)
-
-
-@admin.register(Staff)
-class StaffAdmin(UserAdmin):
-    list_display = [
-        "username", "first_name", "last_name", "staff_position", "email"
-    ]
-    fieldsets = UserAdmin.fieldsets + (
-        ("Positions", {"fields": ("position", )}),
-    )
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        ("Personal Info", {"fields": ("first_name", "last_name", "email")}),
-        ("Positions", {"fields": ("position", )}),
-    )
-    list_filter = ["position"]
-    search_fields = ["first_name", "last_name", "username"]
-
-    def get_queryset(self, request):
-        queryset = super().get_queryset(request)
-        return queryset.prefetch_related("position")
-
-    def staff_position(self, obj):
-        return ", ".join([pos.name for pos in obj.position.all()])
 
 
 @admin.register(Task)
