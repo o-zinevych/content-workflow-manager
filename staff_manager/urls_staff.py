ac4095110077ba4bafb7f841ac56906dd48cf84e
Olha <olgazinevych@gmail.com>
2024-07-12 15:58:01 +0300

    refactor: remove staff from paths in urls_staff to avoid duplication

--- staff_manager/urls_staff.py
+++ staff_manager/urls_staff.py
@@ -9,16 +9,16 @@ from .views import (
 )
 
 urlpatterns = [
-    path("staff/", StaffListView.as_view(), name="staff-list"),
-    path("staff/<int:pk>/", StaffDetailView.as_view(), name="staff-detail"),
-    path("staff/create/", StaffCreateView.as_view(), name="staff-create"),
+    path("", StaffListView.as_view(), name="staff-list"),
+    path("<int:pk>/", StaffDetailView.as_view(), name="staff-detail"),
+    path("create/", StaffCreateView.as_view(), name="staff-create"),
     path(
-        "staff/<int:pk>/update/",
+        "<int:pk>/update/",
         StaffUpdateView.as_view(),
         name="staff-update",
     ),
     path(
-        "staff/<int:pk>/delete/",
+        "<int:pk>/delete/",
         StaffDeleteView.as_view(),
         name="staff-delete",
     ),
