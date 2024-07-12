867ae1fa263351b3e55cc2d33f34bf3b82426de4
Olha <olgazinevych@gmail.com>
2024-07-12 15:46:44 +0300

    test: move staff admin panel test to staff_manager app

--- content/tests/test_admin.py
+++ content/tests/test_admin.py
@@ -4,32 +4,8 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
-from content.admin import StaffAdmin, TaskAdmin
-from content.models import Position, Task, ContentType
-
-
-class StaffAdminPanelTest(TestCase):
-    def setUp(self):
-        self.position1 = "Moderator"
-        self.position2 = "Administrator"
-        position1 = Position.objects.create(name=self.position1)
-        position2 = Position.objects.create(name=self.position2)
-        self.admin_user = get_user_model().objects.create_superuser(
-            username="test_admin",
-            password="test1234",
-        )
-        self.admin_user.position.add(position1, position2)
-        self.client.force_login(self.admin_user)
-
-    def test_staff_queryset(self):
-        response = self.client.get(reverse("admin:content_staff_changelist"))
-        queryset = response.context["cl"].queryset
-        self.assertIn("position", queryset._prefetch_related_lookups)
-
-    def test_staff_position_field(self):
-        admin_instance = StaffAdmin(model=get_user_model(), admin_site=None)
-        staff_position = admin_instance.staff_position(self.admin_user)
-        self.assertEqual(staff_position, f"{self.position2}, {self.position1}")
+from content.admin import TaskAdmin
+from content.models import Task, ContentType
 
 
 class TaskAdminPanelTest(TestCase):
