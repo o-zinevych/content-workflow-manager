48296494b482330e978248db1896bc88b4262d4f
Olha <olgazinevych@gmail.com>
2024-07-13 20:52:00 +0300

    chore: add localhost to ALLOWED_HOSTS

--- content_workflow_manager/settings.py
+++ content_workflow_manager/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["content-workflow-manager.onrender.com"]
+ALLOWED_HOSTS = ["localhost", "content-workflow-manager.onrender.com"]
 
 
 # Application definition
